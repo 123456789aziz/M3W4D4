de2063cb2ea9cf4f5a4705e48c025283b3bbf901
azizMod <azizbaabdou4@gmail.com>
2024-07-15 00:28:07 +0200

    created index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    breakPoints: {
+        320: {
+            slidesPerView: 1
+        },
+        480: {
+            slidesPerView: 3
+        },
+        640: {
+            slidesPerView: "auto"
+        }
+
+    }
+})
